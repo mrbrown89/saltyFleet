4972e7ef936322e87a2c1b7165b47e3f6c565de9
Matt <mbrown_89@icloud.com>
2026-04-08 21:25:38 +0100

    Wrong script

--- bootStrap/bootStrap.sh
+++ bootStrap/bootStrap.sh
@@ -4,30 +4,6 @@ set -e
 
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
-install_xcode_tools() {
-  echo ">>> Ensuring Xcode Command Line Tools (if needed)..."
-
-  if ! xcode-select -p >/dev/null 2>&1; then
-    touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
-    softwareupdate -l >/dev/null 2>&1 || true
-
-    CLT_LABEL="$(
-      softwareupdate -l 2>&1 |
-        awk -F'*' '/Command Line Tools for Xcode/{print $2}' |
-        sed 's/^ Label: //;s/^ *//;q' || true
-    )"
-
-    if [[ -n "${CLT_LABEL}" ]]; then
-      sudo softwareupdate -i "${CLT_LABEL}" --verbose
-      sudo xcode-select --switch /Library/Developer/CommandLineTools
-    fi
-
-    rm -f /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
-  else
-    echo ">>> Command Line Tools already installed."
-  fi
-}
-
 install_fleetctl() {
   if [ ! -f "$HOME/.fleetctl/fleetctl" ]; then
     echo "Installing fleetctl..."
@@ -66,7 +42,6 @@ fleet_config() {
 }
 
 main() {
-  install_xcode_tools
   install_fleetctl
   start_fleet
   start_bucket
